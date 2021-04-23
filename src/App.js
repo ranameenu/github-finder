959937e05a99ba06b5b1f13f975d4fc3fb378967
meenu <meenu.rana@successive.tech>
2021-04-23 20:22:09 +0530

    components, props, Proptypes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
-  month = () => 'April';
   render() {
-    const name = 'John Doe';
-    const foo = () => 'Bar';
-    const loading = false;
-    const showname = true;
-
     return (
       <div className="app">
-        {loading ? <h1>Loading...</h1> : showname && <h4>Hi I'm {name}</h4>}
+        <Navbar />
       </div>
     );
   }
