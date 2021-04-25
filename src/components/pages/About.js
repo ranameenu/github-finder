317437f5627c977bbba3f37b23405bb038004a75
meenu <meenu.rana@successive.tech>
2021-04-25 22:56:36 +0530

    Routers

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>Hi It is About the Application</h1>
+    </Fragment>
+  );
+};
+
+export default About;
